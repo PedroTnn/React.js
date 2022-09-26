ed784c1feafea65a1194c6c6af2c623b374ea872
Pedro T <pedrotnns@gmail.com>
2022-09-26 16:52:53 -0600

    Create React App and Extend products

--- React Course Hooks Routers, Redux,Next js, and Projects/First Example/shop-store/src/App.js
+++ React Course Hooks Routers, Redux,Next js, and Projects/First Example/shop-store/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from "react";
+import React from 'react'
+class App extends Component{
+   state = {
+    productos:[
+      {name:'Tomato', price:1500, img: '/productos/tomato.jpg'},
+      {name:'Lettuce', price:1000, img: '/productos/lettuce.jpg'},
+      {name:'Spinach', price:2000, img: '/productos/spinach.jpg'},
+    ]
+  }
+  render(){
+    return(
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    )
+  }
 }
+
 
 export default App;
