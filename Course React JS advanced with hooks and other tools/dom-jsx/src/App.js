6b5d8b34178f31362acd86b1ff86fb09e252dadc
Pedro T <pedrotnns@gmail.com>
2022-09-27 17:34:35 -0600

    Create component event catcher

--- Course React JS advanced with hooks and other tools/dom-jsx/src/App.js
+++ Course React JS advanced with hooks and other tools/dom-jsx/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
+/*
 function retornarAleatorio() {
   return Math.trunc(Math.random()*10)
 }
+*/
+
+function Mostrar(titulo){
+  return (
+  <h1>
+    {titulo}
+  </h1>)
+}
+
 function App() {
+  /*
   const siglo = 21;
   const persona={
     nombre: 'Juan',
@@ -25,6 +36,32 @@ function App() {
          3 + 3 = {3 + 3}
     </div>
   )
+  
+
+  const buscadores = ['http://www.google.com',
+    'http://www.bing.com',
+    'http://www.yahoo.com'];
+  return (
+    <div>
+      <a href={buscadores[0]}>Google</a><br />
+      <a href={buscadores[1]}>Bing</a><br />
+      <a href={buscadores[2]}>Yahoo</a><br />
+    </div>
+  );
+  */
+return(
+
+  <div>
+    <div>
+{Mostrar('Hola Mundo')}
+</div>
+<div>
+{Mostrar('Fin del mundo, soy un capo papa')}
+    </div>
+  </div>
+)
+
+  
 }
 
 export default App;
