029a91a7fb29162d67b1811416b9a9cb9617b899
Pedro T <pedrotnns@gmail.com>
2022-09-28 17:30:25 -0600

    Example five random int Complete

--- Course React JS advanced with hooks and other tools/4.hook-usestate/src/App.js
+++ Course React JS advanced with hooks and other tools/4.hook-usestate/src/App.js
@@ -2,22 +2,37 @@
 //import React from 'react';
 import React,{useState} from 'react'
 
-
+//Hacer una aplicacion que pueda elegir si se quiere una multiplicacion
+//una division o una suma 
 
 function App () {
-  const  [numero,setNumero]=useState(0)
+  //numero de refencia y la funcion que cambia el numero
+
   function generador(){
-    const entero= Math.trunc(Math.random()*10);
-    setNumero(entero)
+    const numeros = new Array(5);
+for (let i = 0; i < numeros.length; i++) {
+  numeros[i]=Math.trunc(Math.random()*10)
+  setNumeros(numeros)
+}
+    const entero = Math.trunc(Math.random()*10);
+    
   }
+  const [numerosAleatorios,setNumeros]=useState([0,0,0,0,0])
+
+/*function suma(valor1,valor2){
+  const total = valor1 + valor2
+  return total*/
+    
+
 
   
   return (
     <div>
-      <p>Numero aleatorio : {numero}</p>
-      <button onClick={generador}>Generador</button>
+    <p>Número aleatorio</p>
+      {numerosAleatorios.map(n => (<p>{n}</p>))}
+      <button onClick={generador}>Generar número aleatorio</button>
     </div>
   );
-}
+  }
 
 export default App;
