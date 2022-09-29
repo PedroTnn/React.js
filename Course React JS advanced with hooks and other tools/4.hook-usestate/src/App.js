445dbdda832f1b16bb2425f2f79cdedb3d588d4a
Pedro T <pedrotnns@gmail.com>
2022-09-29 13:36:30 -0600

    shop create

--- Course React JS advanced with hooks and other tools/4.hook-usestate/src/App.js
+++ Course React JS advanced with hooks and other tools/4.hook-usestate/src/App.js
@@ -7,30 +7,42 @@ import React,{useState} from 'react'
 
 function App () {
   //numero de refencia y la funcion que cambia el numero
-
+/*
   function generador(){
+    //Creando el array con 5 casillas
     const numeros = new Array(5);
+    //Este array tiene 5 posiciones las cuales 
+    //
 for (let i = 0; i < numeros.length; i++) {
   numeros[i]=Math.trunc(Math.random()*10)
   setNumeros(numeros)
-}
-    const entero = Math.trunc(Math.random()*10);
-    
+} 
   }
-  const [numerosAleatorios,setNumeros]=useState([0,0,0,0,0])
-
+  const [numerosAleatorios, setNumeros]=useState([0,0,0,0,0])
+*/
 /*function suma(valor1,valor2){
   const total = valor1 + valor2
   return total*/
-    
-
-
-  
-  return (
-    <div>
-    <p>Número aleatorio</p>
+   /* 
+   <p>Número aleatorio</p>
       {numerosAleatorios.map(n => (<p>{n}</p>))}
       <button onClick={generador}>Generar número aleatorio</button>
+      */
+
+function sumador() {
+    let numero = 1
+    let total = 0
+    total = numero +  numero
+
+  setInicial(numero)
+}
+
+const [inicial,setInicial]=useState(0)
+  return (
+    <div>
+            <p>Sumador de clicks {inicial}</p>
+                   
+            <button onClick={sumador}> Generador</button>
     </div>
   );
   }
