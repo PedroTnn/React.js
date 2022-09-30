812a674442ff9dc469fc2a9f4e6415b4bc0a753e
Pedro T <pedrotnns@gmail.com>
2022-09-30 13:41:02 -0600

    Style create DADO

---
+++ Course React JS advanced with hooks and other tools/6.introduccion-to-components/src/Dado.js
@@ -0,0 +1,11 @@
+import './Dado.css' 
+import React from 'react'
+function Dado(propiedades){
+    return(
+        <div className='dado-cuadradoo'>
+           {propiedades,valor}
+        </div>
+    )
+}
+
+export default Dado
